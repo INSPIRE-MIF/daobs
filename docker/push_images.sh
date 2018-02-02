03ea24890e79d8e9766dc91fa76499f1393bb1f0
doublebyte1 <doublebyte@gmail.com>
2018-02-02 13:04:59 +0100

    - added cerebro image with built-in configuration - enabled password configuration of cerebro on docker-compose

--- docker/push_images.sh
+++ docker/push_images.sh
@@ -2,3 +2,4 @@ docker push inspiremif/daobs-eea-dashboard-sandbox
 docker push inspiremif/daobs-eea-dashboard-official
 docker push inspiremif/elasticsearch
 docker push inspiremif/kibana
+docker push inspiremif/cerebro
