9b2f6d919c821e9dee05e28d9096521307158bdf
doublebyte1 <doublebyte@gmail.com>
2018-02-05 13:14:09 +0100

    - kibana.yml enable string replacement on an intermediate file

--- docker/kibana/docker-entry.sh
+++ docker/kibana/docker-entry.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-cd /usr/share/kibana/
+set -e
 
-/usr/share/kibana/bin/kibana-plugin remove x-pack
+cd /usr/share/kibana/
 
 echo $SERVER_BASEPATH
 echo $REGIONMAP_LAYERS_URL
 #echo $USERNAME
 echo $PASSWORD
 
+cp /kibana.yml /usr/share/kibana/config/kibana.yml
+
 sed "s#SERVER_BASEPATH#$SERVER_BASEPATH#g" -i /usr/share/kibana/config/kibana.yml
 sed "s#REGIONMAP_LAYERS_URL#$REGIONMAP_LAYERS_URL#g" -i /usr/share/kibana/config/kibana.yml
-#sed "s#USERNAME#$USERNAME#g" -i /usr/share/kibana/config/kibana.yml
 sed "s#PASSWORD#$PASSWORD#g" -i /usr/share/kibana/config/kibana.yml
 
-/usr/share/kibana/bin/kibana
+/usr/share/kibana/bin/kibana-plugin remove x-pack
+
+exec /usr/share/kibana/bin/kibana --cpu.cgroup.path.override=/ --cpuacct.cgroup.path.override=/ ${longopts} "$@"
