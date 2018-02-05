b44560302f7ad9fe9eaf019959a929a5499401a9
doublebyte1 <doublebyte@gmail.com>
2018-02-05 11:16:45 +0100

    - fixed es credentials on dashboard and kibana images

--- docker/docker-entry.sh
+++ docker/docker-entry.sh
@@ -5,13 +5,13 @@ cd /daobs-data-dir/datadir/;
 # TODO: only do that if index does not exist.
 elasticdump \
   --input=index-dashboards-mapping.json \
-  --output=http://kibana_server:password@elasticsearch:9200/.dashboards \
+  --output=http://kibana_server:$PASSWORD@elasticsearch:9200/.dashboards \
   --type=mapping \
   -headers='{"Content-Type": "application/json"}'
 
 elasticdump \
   --input=index-dashboards.json \
-  --output=http://kibana_server:password@elasticsearch:9200/.dashboards \
+  --output=http://kibana_server:$PASSWORD@elasticsearch:9200/.dashboards \
   -headers='{"Content-Type": "application/json"}'
 
 echo -e "\e[96mStart tomcat\e[0m"
--- docker/kibana/docker-entry.sh
+++ docker/kibana/docker-entry.sh
@@ -6,12 +6,12 @@ cd /usr/share/kibana/
 
 echo $SERVER_BASEPATH
 echo $REGIONMAP_LAYERS_URL
-echo $USERNAME
+#echo $USERNAME
 echo $PASSWORD
 
 sed "s#SERVER_BASEPATH#$SERVER_BASEPATH#g" -i /usr/share/kibana/config/kibana.yml
 sed "s#REGIONMAP_LAYERS_URL#$REGIONMAP_LAYERS_URL#g" -i /usr/share/kibana/config/kibana.yml
-sed "s#USERNAME#$USERNAME#g" -i /usr/share/kibana/config/kibana.yml
+#sed "s#USERNAME#$USERNAME#g" -i /usr/share/kibana/config/kibana.yml
 sed "s#PASSWORD#$PASSWORD#g" -i /usr/share/kibana/config/kibana.yml
 
 /usr/share/kibana/bin/kibana
