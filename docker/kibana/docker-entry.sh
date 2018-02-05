f05dff93bbbe2dae03acbc5b380e35d70aeffd4c
doublebyte1 <doublebyte@gmail.com>
2018-02-05 10:54:59 +0100

    - fixed credential names on kibana

--- docker/kibana/docker-entry.sh
+++ docker/kibana/docker-entry.sh
@@ -6,12 +6,12 @@ cd /usr/share/kibana/
 
 echo $SERVER_BASEPATH
 echo $REGIONMAP_LAYERS_URL
-echo $ELASTICSEARCH_USERNAME
-echo $ELASTICSEARCH_PASSWORD
+echo $USERNAME
+echo $PASSWORD
 
 sed "s#SERVER_BASEPATH#$SERVER_BASEPATH#g" -i /usr/share/kibana/config/kibana.yml
 sed "s#REGIONMAP_LAYERS_URL#$REGIONMAP_LAYERS_URL#g" -i /usr/share/kibana/config/kibana.yml
-sed "s#ELASTICSEARCH_USERNAME#$ELASTICSEARCH_USERNAME#g" -i /usr/share/kibana/config/kibana.yml
-sed "s#ELASTICSEARCH_PASSWORD#$ELASTICSEARCH_PASSWORD#g" -i /usr/share/kibana/config/kibana.yml
+sed "s#USERNAME#$USERNAME#g" -i /usr/share/kibana/config/kibana.yml
+sed "s#PASSWORD#$PASSWORD#g" -i /usr/share/kibana/config/kibana.yml
 
 /usr/share/kibana/bin/kibana
