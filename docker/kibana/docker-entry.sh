777d97d2016f70336d5eb86f9e646f17de4560a2
doublebyte1 <doublebyte@gmail.com>
2018-02-05 16:40:36 +0100

    -  remove x-pack on the Dockerfile to avoid this action on restarts

--- docker/kibana/docker-entry.sh
+++ docker/kibana/docker-entry.sh
@@ -10,6 +10,4 @@ cp /kibana.yml /usr/share/kibana/config/kibana.yml
 
 sed "s#PASSWORD#$PASSWORD#g" -i /usr/share/kibana/config/kibana.yml
 
-/usr/share/kibana/bin/kibana-plugin remove x-pack
-
 exec /usr/share/kibana/bin/kibana --cpu.cgroup.path.override=/ --cpuacct.cgroup.path.override=/ ${longopts} "$@"
