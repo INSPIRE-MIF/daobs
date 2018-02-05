b5461938d7459094f6fea25916c5e85f559278c8
doublebyte1 <doublebyte@gmail.com>
2018-02-05 17:02:47 +0100

    - added nginx image with built in configurations

--- docker/push_images.sh
+++ docker/push_images.sh
@@ -2,4 +2,5 @@ docker push inspiremif/daobs-eea-dashboard-sandbox
 docker push inspiremif/daobs-eea-dashboard-official
 docker push inspiremif/elasticsearch
 docker push inspiremif/kibana
+docker push inspiremif/nginx
 docker push inspiremif/cerebro
