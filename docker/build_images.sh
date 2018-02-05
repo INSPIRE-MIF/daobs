9b3efcb5361f9d08f38598db526387bc6716bf0b
doublebyte1 <doublebyte@gmail.com>
2018-02-05 18:03:27 +0100

    - fixed webapp.url to run application on root - added server.basePath to kibana configuration

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -5,6 +5,9 @@ NC='\033[0m' # No Color
 
 echo -e "${CYAN}Building wars${NC}";
 cd ..
+
+rm ./tasks/etf-validation-checker/ETF1.5.zip
+
 mvn clean install -Peea-inspire-dashboard -Drelax -DskipTests
 mvn clean install -Peea-inspire-official -Drelax -DskipTests
 
